a597719b33a9932e1d1c3c0a08b2d8ca3745a513
Matheus Alefe <matheus.orvati@hotmail.com>
2022-06-14 15:36:41 -0300

    Correção de bug: Renderização (widget)

--- web/src/components/Widget.tsx
+++ web/src/components/Widget.tsx
@@ -1,11 +1,9 @@
-import { ChatTeardropDots } from 'phosphor-react'
+import { ChatTeardropDots } from 'phosphor-react';
 
 export function Widget() {
-    return (
-      <div className='position-absolute bottom-4 right-4 text-white'>
-        <button className="bg-brand-500">
-          <ChatTeardropDots />
-        </button>
-      </div>
+    return( 
+      <div className='absolute bottom-5 right-5'>
+        <button className='bg-brand-500 px-3 w-12 h-12 rounded-full text-white'><ChatTeardropDots className='w-6 h-6' /></button>
+      </div> 
     )
   }
--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -5,7 +5,7 @@ module.exports = {
     extend: {
       colors:{
         brand:{
-          500: '8257e6'
+          500: '#8257e6'
         }
       }
     },
