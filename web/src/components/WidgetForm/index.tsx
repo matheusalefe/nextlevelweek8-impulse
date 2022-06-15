17a9213a5da51bf3bbdddf3ee2c4cd0b5fc1ccc1
Matheus Alefe <matheus.orvati@hotmail.com>
2022-06-15 11:52:35 -0300

    Inserir estrutura  de passos e comunicação intercomponentes via funções

--- web/src/components/WidgetForm/index.tsx
+++ web/src/components/WidgetForm/index.tsx
@@ -1,11 +1,12 @@
 import { useState } from "react";
-import { CloseButton } from "./CloseButton"
-import bugImageUrl from '../assets/bug.svg'
-import ideaImageUrl from '../assets/idea.svg'
-import thoughtImageUrl from '../assets/thought.svg'
+import { CloseButton } from "../CloseButton"
+import bugImageUrl from '../../assets/bug.svg'
+import ideaImageUrl from '../../assets/idea.svg'
+import thoughtImageUrl from '../../assets/thought.svg'
+import { FeedbackTypeStep } from "./Steps/FeedbackTypeStep";
 
 
-const feedbackTypes = {
+export const feedbackTypes = {
     BUG :{
         title: 'Problema',
         image: {
@@ -29,7 +30,9 @@ const feedbackTypes = {
     },
 }
 
-type FeedbackType = keyof typeof feedbackTypes;
+export type FeedbackType = keyof typeof feedbackTypes;
+
+
 
 export function WidgetForm(){
     const [feedbackType, setFeedbackType ] = useState<FeedbackType | null>(null)
@@ -42,22 +45,9 @@ export function WidgetForm(){
                 <span className="text-xl lending-6">Deixe o seu feedback</span>
             </header>
 
-            {!feedbackType ? (<div className="flex py-8 gap-2 w-full">
-                    { Object.entries(feedbackTypes).map(([key, value]) => {
-                        
-                        return (
-                            <button
-                            key={key}
-                            className="bg-zinc-800 rounded-lg py-5 w-24 flex flex-col items-center gap-2 border-2 border-transparent hover:border-brand-500 focus:border-brand-500 focus:outline-none"
-                            onClick={() => setFeedbackType(key as FeedbackType)}
-                            type="button"
-                            >
-                                <img src={value.image.source} alt={value.image.alt} />
-                                <span>{value.title}</span>
-                            </button>
-                        );
-                    }) }
-                </div>) : (
+            {!feedbackType ? (
+                <FeedbackTypeStep onFeedbackTypeChanged={setFeedbackType} />
+                ) : (
                     <p>Hello, world!</p>
                 )
                 }
